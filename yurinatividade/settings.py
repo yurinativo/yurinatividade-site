3c8e589c7308c460b9b270ddbec91bc5c172f0fc
Yuri Natividade <yurinativo@gmail.com>
2018-09-03 09:35:00 -0300

    removendo blog2

--- yurinatividade/settings.py
+++ yurinatividade/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$(rit!qlhmi%xmh$s*#z6s!tla0@5owc0m+o7orwmg)*@7sx^^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
