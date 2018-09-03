5f7074a2291c91dc306f57d5b01f4dfd9781d55b
Yuri Natividade <yurinativo@gmail.com>
2018-09-03 14:47:34 -0300

    Criando views

--- yurinatividade/settings.py
+++ yurinatividade/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$(rit!qlhmi%xmh$s*#z6s!tla0@5owc0m+o7orwmg)*@7sx^^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
@@ -121,5 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
