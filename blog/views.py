79f45bed5672927efa8457ef5df2211d0316de0c
Yuri Natividade <yurinativo@gmail.com>
2018-09-03 16:17:46 -0300

    Corrigindo não exibição de dados

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    posts = Post.objects.all().order_by('published_date') 
     return render(request, 'blog/post_list.html', {'posts':posts})
     
 def post_detail(request, pk):
